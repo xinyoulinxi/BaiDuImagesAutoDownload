89c3825aa5436573787e3f169e31299af85580f3
xinyoulinxi <1135479396@qq.com>
2019-01-05 14:47:45 +0800

    添加网络源码获取文件

---
+++ url_get.py
@@ -0,0 +1,13 @@
+import urllib.request
+def get_html(httpUrl):
+	page = urllib.request.urlopen( httpUrl )#打开网页
+	htmlCode = page.read( )#读取网页
+	return htmlCode
+
+'''
+def store_html_code(html_code,file_name):
+	f_html=open(file_name,'w',encoding='utf-8')
+	html_str=html_code.decode(encoding = "utf-8")#将二进制码解码为utf-8编码，即str
+	f_html.write(html_str)#存入
+	f_html.close()
+'''
